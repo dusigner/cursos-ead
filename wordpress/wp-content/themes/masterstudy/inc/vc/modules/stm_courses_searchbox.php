eacb20b2565d6a4dc7f087391b04b5f57fe30d7b
Eduardo Soares <eduardo@visie.com.br>
2020-06-29 20:28:59 -0300

    first commit

---
+++ wordpress/wp-content/themes/masterstudy/inc/vc/modules/stm_courses_searchbox.php
@@ -0,0 +1,40 @@
+<?php
+
+add_action('vc_after_init', 'stm_courses_searchbox_vc');
+
+function stm_courses_searchbox_vc()
+{
+	vc_map(array(
+		'name'   => esc_html__('STM Courses Search box', 'masterstudy'),
+		'base'   => 'stm_courses_searchbox',
+		'icon'   => 'stm_courses_searchbox',
+		'description' => esc_html__('Search in LMS Courses', 'masterstudy'),
+		'category' =>array(
+			esc_html__('Content', 'masterstudy'),
+		),
+		'params' => array(
+            array(
+                'type'       => 'dropdown',
+                'heading'    => __( 'Style', 'masterstudy' ),
+                'param_name' => 'style',
+                'value'      => array(
+                    'Style 1' => 'style_1',
+                    'Style 2' => 'style_2',
+                ),
+                'std' => 'style_1'
+            ),
+			array(
+				'type'       => 'css_editor',
+				'heading'    => esc_html__('Css', 'masterstudy'),
+				'param_name' => 'css',
+				'group'      => esc_html__('Design options', 'masterstudy')
+			)
+		)
+	));
+}
+
+if (class_exists('WPBakeryShortCode')) {
+	class WPBakeryShortCode_Stm_Courses_Searchbox extends WPBakeryShortCode
+	{
+	}
+}
